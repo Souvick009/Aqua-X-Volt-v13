ab8ced93d58132db4fb327574d6d3f89354f8687
Souvick009 <souvick8670527410@gmail.com>
2022-01-13 03:40:20 +0530

    Update ping.js

--- commands/Info/ping.js
+++ commands/Info/ping.js
@@ -15,12 +15,12 @@ module.exports = {
   run: async (bot, message, args) => {
     if (message.type == "APPLICATION_COMMAND") {
       var m = await message.reply({ content: "Loading.....", fetchReply: true })
-      await message.editReply(`🏓Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ws.ping)}ms`)
+      await message.editReply(`🏓Latency is \`${Math.round(bot.ws.ping)}ms\`.`)
 
     } else {
       message.reply('Loading data').then(async (msg) => {
         msg.delete()
-        message.reply(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ws.ping)}ms`);
+        message.reply(`🏓Latency is \`${Math.round(bot.ws.ping)}ms\`.`);
       })
     }
   }
